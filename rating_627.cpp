7a19af60f74a0452fc2723ea302380c56c348173
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-22 00:26:10 +0600

    codechef

--- rating_627.cpp
+++ rating_627.cpp
@@ -6,14 +6,7 @@ int main(){
     for(int i=0;i<t;i++){
         int a,b,c,d;
         cin>>a>>b>>c>>d;
-        int k=a+c;
-        int l=a+d;
-        int j=b+c;
-        int m=b+d;
-        if(k>=l && k>=j && k>=m) cout<<k<<endl;
-        else if(l>=k && l>=j && l>=m) cout<<l<<endl;
-        else if(j>=k && j>=l && j>=m) cout<<j<<endl;
-        else cout<<m<<endl;
+        cout<<max(a,b)+max(c,d)<<endl;
     }
     return 0;
 }
