f135b92358349abce7ac963bc05bdf0486fabd60
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-30 03:17:06 +0600

    codechef

---
+++ rating_697.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int w,x,y,z;
+        cin>>w>>x>>y>>z;
+        if((x+y==w) || (y+z==w) || (x+z==w) || (x+y+z==w) || x==w || y==w || z==w) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
