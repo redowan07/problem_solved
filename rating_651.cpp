1a786b5c176ab335a4c041d822847452234b95b2
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-29 17:00:45 +0600

    codechef

---
+++ rating_651.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x==y) cout<<0<<endl;
+        else if(x<y) {
+            if((y-x)%8==0) cout<<(y-x)/8<<endl;
+            else cout<<((y-x)/8)+1<<endl;
+        }
+    }
+    return 0;
+}
