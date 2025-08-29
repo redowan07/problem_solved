eb365a48d48491573eb399cab89cc2b3d10ba597
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-30 00:05:14 +0600

    codechef

---
+++ rating__668.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int k=sqrt(x);
+        cout<<k<<endl;
+    }
+    return 0;
+}
