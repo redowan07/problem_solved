644f0180c0863a814d1c176f71aa12305f6fac58
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-30 01:24:43 +0600

    codechef

---
+++ rating_690.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x1,x2,y1,y2;
+        cin>>x1>>y1>>x2>>y2;
+        int a=abs(x1-x2),b=abs(y1-y2);
+        cout<<max(a,b)<<endl;
+    }
+    return 0;
+}
